236f799d5c7d7fa860c1bf343de0aaa9e549e258
Nikolay <81419030+Nikolay1580@users.noreply.github.com>
2023-09-30 20:31:23 +0200

    Initial Commit

---
+++ Metals/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Metals
+//
+//  Created by Nikolay Tsonev on 30/09/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
